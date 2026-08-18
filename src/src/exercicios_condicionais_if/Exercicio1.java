02328750a833c07c65be144f63c7d55410a70693
helenaorth123-maker <helenaorth123@gmail.com>
2026-08-18 08:43:34 -0300

    if

---
+++ src/src/exercicios_condicionais_if/Exercicio1.java
@@ -0,0 +1,31 @@
+package exercicios_condicionais_if;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Informe sua idade:");
+		String idade = leia.next();
+
+		if(idade >= 18){
+			System.out.println("Maior de idade")
+		}
+
+		else{
+			System.out.println("--------")
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}

e5eef24dee27c44a948798f4a85bd40a6684ba61
Helena <helenaorth123@gmail.com>
2026-09-10 08:03:56 -0300

    Remove tabuada logic from Exercicio5.java
    Removed the tabuada logic from the main method.

--- src/exercicios_lacos_repeticao/Exercicio5.java
+++ src/exercicios_lacos_repeticao/Exercicio5.java
@@ -8,20 +8,6 @@ public class Exercicio5 {
 
         Scanner leia = new Scanner(System.in);
 
-        int tab = 1;
-        int mult = 0;
-		
-		System.out.println("Informe um número para a tabuada:");
-        int num = leia.nextInt();
-		
-		while(tab <= 10)
-        {
-            mult = num * tab;
-
-            System.out.println(num+" X "+tab+" = "+mult);
-            tab++;
-        }
-
 	}
 
 }
